f32ac064980b81de619dbe4897f9acc8e2b9c31e
csederman <crs7240@gmail.com>
2023-11-22 12:26:10 -0700

    Fixed caching of PubCHEM properties

--- screendl/preprocessing/data/pubchem.py
+++ screendl/preprocessing/data/pubchem.py
@@ -24,17 +24,17 @@ PUBCHEM_DEFAULT_PROPERTIES = [
 ]
 
 
-def _load_cached_properties(fs_cache: Path) -> t.Dict[str, t.Any]:
+def _load_cached_properties(cache_path: Path) -> t.Dict[str, t.Any]:
     """Loads cached PubCHEM properties."""
     cached_props = dict()
-    if fs_cache.exists():
-        with open(fs_cache, "r") as fh:
+    if cache_path.exists():
+        with open(cache_path, "r") as fh:
             cached_props.update(json.load(fh))
     return cached_props
 
 
 def fetch_pubchem_properties(
-    pubchem_cids: t.List[str], fs_cache: t.Optional[str | Path]
+    pubchem_cids: t.List[str], cache_path: t.Optional[str | Path] = None
 ) -> pd.DataFrame:
     """Queries PubCHEM properties for the specified PubCHEM compound ids.
 
@@ -47,35 +47,35 @@ def fetch_pubchem_properties(
     -------
         A `pd.DataFrame` of PubCHEM properties
     """
-    cached_props = _load_cached_properties(Path(fs_cache))
-
     properties_str = ",".join(PUBCHEM_DEFAULT_PROPERTIES)
     url_fmt = f"{PUBCHEM_BASE_URL}/{{}}/property/{properties_str}/JSON"
 
-    query_results = []
-    num_requests = 0
+    query_props = {}
+    cached_props = {}
+    if isinstance(cache_path, (str, Path)):
+        cached_props = _load_cached_properties(Path(cache_path))
+
+    n_requests = 0
     for cid in tqdm(pubchem_cids, desc="Fetching PubCHEM properties"):
         if cid in cached_props:
-            query_results.append(cached_props[cid])
+            query_props[cid] = cached_props[cid]
             continue
 
-        # fetch the properties from PubCHEM
         try:
             resp = requests.get(url_fmt.format(cid))
-            resp_json = resp.json()
-            props = resp_json["PropertyTable"]["Properties"][0]
-
-            query_results.append(props)
-            cached_props[cid] = props
+            query_props[cid] = resp.json()["PropertyTable"]["Properties"][0]
 
         except Exception as e:
+            # FIXME: add pass after fail
             raise e
 
-        num_requests += 1
-        if num_requests % 5 == 0:
-            time.sleep(0.5)  # avoid rate limiting errors
+        n_requests += 1
+        if n_requests % 5 == 0:
+            time.sleep(0.5)  # avoid rate limiting errors from PubCHEM
 
-    with open(fs_cache, "w", encoding="utf-8") as fh:
-        json.dump(cached_props, fh, ensure_ascii=False, indent=4)
+    if isinstance(cache_path, (str, Path)):
+        cached_props.update(query_props)
+        with open(cache_path, "w", encoding="utf-8") as fh:
+            json.dump(cached_props, fh, ensure_ascii=False, indent=4)
 
-    return pd.DataFrame(query_results)
+    return pd.DataFrame.from_dict(query_props, orient="index")
