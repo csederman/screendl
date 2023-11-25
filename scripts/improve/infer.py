e6a357cc917bd4668b03b4a9cbeabc4c5e571568
csederman <crs7240@gmail.com>
2023-11-25 17:15:50 +0000

    Minor bug fixes

--- scripts/improve/infer.py
+++ scripts/improve/infer.py
@@ -15,17 +15,25 @@ import benchmark as bmk
 import tensorflow.keras.backend as K  # pyright: ignore[reportMissingImports]
 
 from pathlib import Path
+from tensorflow import keras
 
 from cdrpy.data import Dataset
+from cdrpy.mapper import BatchedResponseGenerator
+
+from screendl.utils import evaluation as eval_utils
+
+if t.TYPE_CHECKING:
+    from cdrpy.feat.encoders import PandasEncoder
+    from cdrpy.feat.transformers import GroupStandardScaler
+    from sklearn.preprocessing import StandardScaler
 
 
 GParams = t.Dict[str, t.Any]
+LabelScaler = t.Union[StandardScaler, GroupStandardScaler]
 
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 init_params = bmk.make_param_initializer(file_path)
-
-
 bmk.configure_session()
 
 
@@ -45,10 +53,32 @@ def load_test_data(g_params: GParams) -> Dataset:
     return D.select(split_dict["train"], name="test")
 
 
-def preprocess_data(g_params: GParams, test_ds: Dataset) -> Dataset:
+def preprocess_data(g_params: GParams, D: Dataset) -> Dataset:
     """Apply preprocessing pipeline."""
     output_dir = Path(g_params["output_dir"])
+    norm_method = g_params["label_norm_method"]
+
     print(os.listdir(g_params["output_dir"]))
+
+    with open(output_dir / "label_scaler.pkl", "rb") as fh:
+        label_scaler: LabelScaler = pickle.load(fh)
+
+    Y = D.obs[["label"]]
+    if norm_method == "grouped":
+        D.obs["label"] = label_scaler.transform(Y, groups=D.obs["drug_id"])
+    elif norm_method == "global":
+        D.obs["label"] = label_scaler.transform(Y)
+    else:
+        raise ValueError(f"Unsupported label normalization method ({norm_method})")
+
+    # normalize gene expression features
+    with open(output_dir / "exp_scaler.pkl", "rb") as fh:
+        exp_scaler: StandardScaler = pickle.load(fh)
+
+    exp_enc: PandasEncoder = D.cell_encoders["exp"]
+    exp_enc.data[:] = exp_scaler.transform(exp_enc.data.values)
+
+    return D
 
 
 def infer(g_params: GParams) -> None:
@@ -56,7 +86,14 @@ def infer(g_params: GParams) -> None:
     data_dir = Path(g_params["data_dir"])
     output_dir = Path(g_params["output_dir"])
 
-    test_ds = load_test_data(g_params)
+    D = load_test_data(g_params)
+    D = preprocess_data(g_params, D)
+
+    model = keras.models.load_model(output_dir / "model")
+
+    gen = BatchedResponseGenerator(D, g_params["batch_size"])
+    preds = model.predict(gen.flow_from_dataset(D))
+    result = eval_utils.make_pred_df(D, preds)
 
 
 def main() -> None:
