a0f44e984da29bc4de3579801aa0e64bf013ecf5
csederman <crs7240@gmail.com>
2026-03-28 14:06:52 -0600

    Refactor dep groups

--- setup.py
+++ setup.py
@@ -8,27 +8,39 @@ requirements = [
     "numpy >= 1.21",
     "pandas >= 2.0.3",
     "openpyxl",
-    # "tensorflow",
-    "tensorflow-probability[tf]",
     "scikit-learn",
     "omegaconf",
     "tqdm",
-    # bump some of these to "preprocess" group
-    "rdkit",
-    "deepchem",
     "cdrpy",
     "scipy >= 1.8.1",
     "mygene >= 3.2.2",
 ]
 
 extras_require = {
+    "gpu": ["tensorflow[and-cuda]"],
+    "preprocess": [
+        "inmoose == 0.2.1",
+        "deepchem",
+        "rdkit",
+    ],
+    "notebooks": [
+        "jupyterlab",
+        "altair",
+        "shap",
+        "statsmodels",
+    ],
     "scripts": [
         "click",
         "hydra-core >= 1.3",
         "hydra-optuna-sweeper >= 1.2.0",
     ],
-    "preprocess": ["inmoose == 0.2.1"],
 }
+
+# default to CPU tensorflow so bare `pip install screendl` still works
+extras_require["all-cpu"] = extras_require["scripts"] + extras_require["notebooks"]
+extras_require["all-gpu"] = (
+    extras_require["gpu"] + extras_require["scripts"] + extras_require["notebooks"]
+)
 
 with open("./test-requirements.txt") as test_reqs_txt:
     test_requirements = [line for line in test_reqs_txt]
@@ -58,11 +70,7 @@ setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     maintainer="Casey Sederman",
     maintainer_email="casey.sederman@hsc.utah.edu",
